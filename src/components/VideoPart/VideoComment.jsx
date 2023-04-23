51695c4c6909507f7a078923d1ff9738fa949a80
Charlotte <charlottemay.234@gmail.com>
2023-04-22 23:41:59 -0700

    front end for adding comment and displaying comments; very ugly..

---
+++ src/components/VideoPart/VideoComment.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+
+const VideoComment = ({ comments }) => {
+  console.log("video comment got comments: ", comments);
+  return (
+    <div>
+      {comments.map((oneComment) => (
+        <div>
+          <Link to={`/`}>
+            <Typography variant="body1" color="#000" fontWeight="bold" p={2}>
+              {oneComment.userName}
+            </Typography>
+          </Link>
+          <Typography variant={{ sm: "body1", md: "body1" }} p={6} color="#000">
+            {oneComment.comment}
+          </Typography>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default VideoComment;
